ceb7d39fd6a337b03073951770c958b7744e182c
s-s01tan <soltanlisoltan.js@gmail.com>
2022-10-21 00:29:11 +0400

    Demo Project Done

---
+++ dist/sum.js
@@ -0,0 +1,8 @@
+"use strict";
+const a = 1;
+const b = 7;
+function sum(first, second) {
+    return first + second;
+}
+const result = sum(1, 7);
+console.log("Sum: ", result);
---
+++ sum.ts
@@ -0,0 +1,9 @@
+const a: number = 1;
+const b: number = 7;
+
+function sum(first: number, second: number): number {
+  return first + second;
+}
+
+const result = sum(1, 7);
+console.log("Sum: ", result);
